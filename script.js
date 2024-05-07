a6a76d8247683e0b4115b3c5d980dc26c3aa3b79
Nikhil Siddardh Ravula <nikhilsiddardhravula@Nikhils-MacBook-Air.local>
2024-05-07 03:37:34 -0500

    remodified script file

--- script.js
+++ script.js
@@ -1,51 +1,53 @@
-let wins=0,loses=0,draws=0;
+scores=JSON.parse(localStorage.getItem('scores'))||{
+    win:0,
+    loss:0,
+    draw:0
+};
 function helper(button){
-    document.querySelector('.you').style.display='none';
-    document.querySelector('.comp').style.display='none';
-    document.querySelector('.draw').style.display='none';
-    let yourChoice=button.className;
-    let compChoice=randomGenerator();
-    document.querySelector('.one').src=`${yourChoice}.png`;
-    document.querySelector('.two').src=`${compChoice}.png`;
+    displayPreset();
+    you=button.className;
+    comp=computerTurn();
     document.querySelector('.status').style.display='block';
-    document.querySelector('.scores').style.display='block';
-    if((yourChoice=='rock' && compChoice=='scissor') || 
-        (yourChoice=='paper' && compChoice=='rock') ||
-        (yourChoice=='scissor' && compChoice=='paper')) youWon();
-    else if(yourChoice==compChoice) draw();
-    else compWon();
+    document.querySelector('.one').src=you+".png";
+    document.querySelector('.two').src=comp+".png";
+    checkWinner(you,comp);
+    displayScore();
+    localStorage.setItem('scores',JSON.stringify(scores));
 }
 
-function randomGenerator(){
-    let choices=['rock','paper','scissor'];
-    return choices[Math.floor(Math.random()*choices.length)];
+function computerTurn(){
+    const choices=['rock','paper','scissor'];
+    return choices[Math.floor(Math.random()*3)];
 }
-
-function youWon(){
-    document.querySelector('.you').style.display='block';
-    wins++;
-    document.querySelector('.wins').innerText=wins;
-}
-
-function compWon(){
+function checkWinner(you,comp){
+    if(you==comp){
+        document.querySelector('.draw').style.display='block';
+        scores.draw++;
+        return 'draw';
+    }
+    if((you=='rock' && comp=='scissor')||(you=='scissor' && comp=='paper') || (you=='paper' && comp=='rock')){
+        document.querySelector('.you').style.display='block';
+        scores.win++;
+        return 'you';
+    } 
     document.querySelector('.comp').style.display='block';
-    loses++;
-    document.querySelector('.loses').innerText=loses;
+    scores.loss++;
+    return 'comp';
 }
-
-function draw(){
-    document.querySelector('.draw').style.display='block';
-    draws++;
-    document.querySelector('.draws').innerText=draws;
-}
-
-document.querySelector('.reset').addEventListener('click',function(){
-    wins=0;
-    loses=0;
-    draws=0;
-    document.querySelector('.scores').style.display='none';
-    document.querySelector('.status').style.display='none';
+function displayPreset(){
+    document.querySelector('.draw').style.display='none';
     document.querySelector('.you').style.display='none';
     document.querySelector('.comp').style.display='none';
-    document.querySelector('.draw').style.display='none';
-});
+}
+function displayScore(){
+    document.querySelector('.scores').style.display='block';
+    document.querySelector('.wins').innerText=scores.win;
+    document.querySelector('.loses').innerText=scores.loss;
+    document.querySelector('.draws').innerText=scores.draw;
+}
+document.querySelector('.reset').addEventListener('click',function(){
+    scores.win=0;
+    scores.loss=0;
+    scores.draw=0;
+    displayScore();
+})
